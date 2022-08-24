4bb3cb98f0f9c06fdce51bf1ba130cc9b38f632e
Alex Bedino <alex.bedino@gmail.com>
2022-08-24 22:24:55 +0700

    Add files via upload

---
+++ conditionalflow.py
@@ -0,0 +1,30 @@
+a = 10
+b = 10
+if a == b: # if the condition is True the code nested is executed
+    print("a equals b")
+
+a = 10
+b = 12
+if a < b:
+    print ("a is less than b")
+
+a = 10
+b = 12
+if a > b: # false statement, code is not executed
+    print ("a is greater than b")
+else:
+    print ("a is not greater than b")
+
+a = 8
+b = 10
+if a > b:
+    print ("a is greater than b")
+elif a == b:
+    print ("a is the same as b")
+else:
+    print ("a is less than b")
+
+a = 9
+b = 10
+if a != b:
+    print ("a is different than b")
